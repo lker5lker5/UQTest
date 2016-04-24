7689f812e3b50d5e803f0cecbb2b6758f8999c87
Vinson <vinson@dyn-118-139-11-254.its.monash.edu.au>
2016-04-24 14:33:44 +1000

    remove the jQuery library and use plain javaScript

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,23 @@
-/**
- *
- */
-(function($){
-    $(function(){
-        $('#p1_answer').on('click', function(){
-            $.ajax({
-                url:'/dataHandler/controllers/getLargestPrimeFactor.php',
-                success: function(result){
-                    alert(result);
-                }
-            });
-        });
-    });
-})(jQuery);
+// (function($){
+//     $(function(){
+//         $('#p1_answer').on('click', function(){
+//             $.ajax({
+//                 url:window.location.href.replace('index.html','') + '/dataHandler/controllers/getLargestPrimeFactor.php',
+//                 success: function(result){
+//                     $('#p1_answer')
+//                 }
+//             });
+//         });
+//     });
+// })(jQuery);
+
+function showAnswer(){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function(){
+        if(4 == xmlHttp.readyState && 200 == xmlHttp.status){
+            var response = xmlHttp.responseText;
+        }
+    }
+    xmlHttp.open("GET", "../../dataHandler/controllers/getLargestPrimeFactor.php");
+    xmlHttp.send();
+}
