d3ea292d6f50e664b4c666261d20636feaadc819
Vinson <vinson@Kaner.local>
2016-04-28 00:06:50 +1000

    copy the whole project

--- dataHandler/dbCfg.php
+++ dataHandler/dbCfg.php
@@ -2,9 +2,9 @@
     header("Content-Type: text/html; charset=utf-8");
 
     $dbcfg = array(
-        'dbhost'  =>  '127.7.182.130:3306', // db's url
-        'dbuser'  =>  'adminZjh4NJ7', // db's username
-        'dbpwd'   =>  'CdDJuR_ckrSb', // db's
+        'dbhost'  =>  'localhost:8889',//'127.7.182.130:3306', // db's url
+        'dbuser'  =>  'root'//'adminZjh4NJ7', // db's username
+        'dbpwd'   =>  'root', // db's
         'dbname'  =>  'pro_sol', //the db we want to use
     );
 
